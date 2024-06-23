c0464e7cb0489f5c83b05e2db098fea9d6115770
rDirkje <reuversdirkje@gmail.com>
2024-06-23 21:48:09 +0200

    pseudocode

--- src/js/endScene.js
+++ src/js/endScene.js
@@ -1,11 +1,16 @@
 import { Scene, Label, Vector, Actor, Color, Font, FontUnit } from "excalibur";
 
+// maak nieuwe klasse aan die ook dient als scene
 export class EndScene extends Scene {
     onInitialize(engine) {
+
+        // zet achtergrond kleur
         this.backgroundColor = Color.Black;
+
+        // voeg een label voor tekst toe
         const endLabel = new Label({
             text: 'You lost :( Try again!',
-            pos: new Vector(engine.drawWidth / 2, engine.drawHeight / 2),
+            pos: new Vector(engine.drawWidth / 2, engine.drawHeight / 2), // geef het midden van het scherm mee als locatie voor de tekst
             color: Color.White,
             font: new Font({
                 family: 'Courier new',
@@ -14,17 +19,22 @@ export class EndScene extends Scene {
                 textAlign: 'center'
             }),
         });
+
+        // voeg het label toe aan de scene
         this.add(endLabel);
 
+        // knopje om de game te restarten
         const endButton = new Actor({
-            pos: new Vector(640, 460),
+            pos: new Vector(640, 460), // locatie midden onder de tekst
             width: 200,
             height: 50,
             color: Color.Magenta
         });
 
+        // voeg de knop toe
         this.add(endButton);
 
+        // tekst voor op de knop aanmaken
         const endButtonLabel = new Label({
             text: 'Restart',
             pos: new Vector(640, 448),
@@ -37,10 +47,12 @@ export class EndScene extends Scene {
             })
         });
 
+        // voeg de tekst toe
         this.add(endButtonLabel);
 
+        // maak de knop werkend
         endButton.on('pointerup', () => {
-            engine.goToScene('game');
+            engine.goToScene('game'); // stuurt door de volgende scene
         });
     }
 }
--- src/js/startScene.js
+++ src/js/startScene.js
@@ -1,33 +1,40 @@
 import { Scene, Label, Vector, Actor, Color, Font, FontUnit } from "excalibur";
 
+// maak nieuwe klasse aan die ook dient als scene
 export class StartScene extends Scene {
     onInitialize(engine) {
 
-        this.backgroundColor = Color.Black;
+        // zet achtergrond kleur
+        this.backgroundColor = Color.Black; 
 
+        // voeg een label voor tekst toe
         const textLabel = new Label({
             text: 'Do not let the enemies get to close to your village, Goodluck!',
-            pos: new Vector(engine.drawWidth / 2, engine.drawHeight / 2),
+            pos: new Vector(engine.drawWidth / 2, engine.drawHeight / 2), // geef het midden van het scherm mee als locatie voor de tekst
             color: Color.White,
             font: new Font({
-                family: 'Courier new',
-                size: 30,
+                family: 'Courier new', 
+                size: 30, 
                 unit: FontUnit.Px,
                 textAlign: 'center'
             }),
         });
-       
-        this.add(textLabel)
 
+        // voeg het label toe aan de scene
+        this.add(textLabel) 
+
+        // knopje om de game te starten
         const startButton = new Actor({
-            pos: new Vector(640, 460),
+            pos: new Vector(640, 460), // locatie midden onder de tekst
             width: 200,
             height: 50,
             color: Color.Magenta
         });
 
+        // voeg de knop toe
         this.add(startButton);
 
+        // tekst voor op de knop aanmaken
         const startButtonLabel = new Label({
             text: 'Start',
             pos: new Vector(640, 448),
@@ -40,10 +47,12 @@ export class StartScene extends Scene {
             })
         });
 
+        // voeg de tekst toe
         this.add(startButtonLabel);
 
+        // maak de knop werkend
         startButton.on('pointerup', () => {
-            engine.goToScene('game');
+            engine.goToScene('game'); // stuurt door de volgende scene
         });
     }
 }
